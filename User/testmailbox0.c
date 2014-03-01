e06d15a693a75b5f981f3e29a8a76e9809f657d7
escraire <escraire@gmail.com>
2014-03-01 14:54:44 -0500

    First commit of the day!

---
+++ User/testmailbox0.c
@@ -0,0 +1,9 @@
+#include "mailbox.h"
+#include <stdio.h>
+
+int main() {
+	int* count;
+	ManageMailbox(false, count);
+	printf("Count is %d!\n", *count);
+	return 0;
+}
---
+++ User/testmailbox1.c
@@ -0,0 +1,34 @@
+/* Alfredo Porras
+ * July 12th, 2011
+ * CS 3013
+ * Project 4 - test program 1
+ * Tests if messages can be sent and received.
+ */
+
+#include "mailbox.h"
+#include <stdio.h>
+
+int main() {
+  pid_t sender;
+  sender = getpid();
+  int childPID = fork();
+  int status;
+  
+  if(childPID == 0){
+    void *msg[128];
+    int len;
+    
+    if(status= RcvMsg(&sender,msg,&len,false))
+		printf("ERROR: %d\n", status);
+    printf("Message received.\n");
+    printf("Message: %s\n", (char *) msg);
+  }
+  else{
+    char mesg[] = "I am your father";
+    printf("Sending Message to child.\n");
+    if (status=SendMsg(childPID, mesg, 17, false)){
+      printf("Send failed with error %d\n", status);
+    }
+  }
+  return 0;
+}
